5d7ef9f7e443b9ab95f24fd476026773ca465296
O-sulloc <bujjaf@gmail.com>
2022-10-27 11:33:35 +0900

    @RequestParam을 이용한 Get 메서드 구현

--- src/main/java/com/example/practice/controller/PracticeController.java
+++ src/main/java/com/example/practice/controller/PracticeController.java
@@ -1,21 +1,34 @@
 package com.example.practice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
 public class PracticeController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "hello world";
     }
 
-    @GetMapping(value = "/request1")
-    public String getVariable2(@RequestParam String name, @RequestParam String email){
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "nnnnname";
+    }
 
-        return String.format("%s %s", name, email);
+    @GetMapping(value = "/variable1/{varaible}")
+    public String getVariable1(@PathVariable String varaible){
+        return varaible;
+    }
+
+    @GetMapping(value = "/variable2/{varaible}")
+    public String getVariable2(@PathVariable String var) {
+        return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getVariable2(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+
+        return String.format("%s %s %s", name, email, organization);
     }
 }
