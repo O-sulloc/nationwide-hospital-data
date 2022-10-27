cc23a9d8c04e27086a92d55e20472961d8d39c1a
O-sulloc <bujjaf@gmail.com>
2022-10-27 11:26:14 +0900

    controller

--- src/main/java/com/example/practice/controller/PracticeController.java
+++ src/main/java/com/example/practice/controller/PracticeController.java
@@ -1,2 +1,21 @@
-package com.example.practice.controller;public class PracticeController {
+package com.example.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PracticeController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+    @GetMapping(value = "/request1")
+    public String getVariable2(@RequestParam String name, @RequestParam String email){
+
+        return String.format("%s %s", name, email);
+    }
 }
